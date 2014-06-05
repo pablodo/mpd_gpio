01c7e66904b4473bf0122cbc39c8250a8198067f
Pablo Díaz Ogni <pablo.diazogni@gmail.com>
2014-06-04 22:22:49 -0300

    Using callbacks from RPIO

--- main.py
+++ main.py
@@ -8,26 +8,20 @@ RPIO.setup(PLAY, RPIO.IN)
 
 client = MPDClient()
 
-time_treshold = 0.5
-last_change = time.time()
 client.connect('localhost', 6600)
 
 def play_pause(gpio_id, val):
     global client
-    global last_change
-    new_time = time.time()
-    if new_time - last_change > time_treshold:
-	status = client.status()
-	if not 'state' in status:
-            return
-        if status['state'] == 'play':
-            client.pause()
-        else:
-	    client.play()
- 	last_change = new_time 
+    status = client.status()
+    if not 'state' in status:
+        return
+    if status['state'] == 'play':
+        client.pause()
+    else:
+        client.play()
 
-RPIO.add_interrupt_callback(PLAY, play_pause, threaded_callback=True)
-RPIO.wait_for_interrupts(threaded=True)
+RPIO.add_interrupt_callback(PLAY, play_pause, debounce_timeout_ms=500)
+RPIO.wait_for_interrupts()
 while True:
     try:
 	time.sleep(30)
